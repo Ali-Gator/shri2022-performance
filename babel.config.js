cc654a89b9193d04ad76430193e0e36510c42cd3
Oleg Gordienov <ogordienov@yandex.ru>
2022-11-03 22:13:45 +0200

    feat: change to only modern browsers

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,7 @@
 const presets = [
   ['@babel/preset-env', {
     targets: {
-      edge: '17',
-      ie: '11',
-      firefox: '50',
-      chrome: '64',
-      safari: '11.1'
+      chrome: '100',
     },
     useBuiltIns: 'entry',
     corejs: '3.21'
